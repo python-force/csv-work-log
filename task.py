1d7bf927557fa287fb3c1af6248fb83284bbf07c
python-force <studio@pythonforce.com>
2018-08-11 19:27:51 -0400

    search by time spent, method for shoing the results

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 class Task:
     filename = "work-log.csv"
@@ -17,11 +18,12 @@ class AddTask(Task):
 
         task_date = input("Enter a date: ")
         task_title = input("Enter a title: ")
+        task_time_spent = input("Enter time spent: ")
         task_notes = input("Enter a notes: ")
 
 
         with open('work-log.csv', 'a') as csvfile:
-            field_names = ['Task Date', 'Task Title', 'Task Notes']
+            field_names = ['Task Date', 'Task Title', 'Time Spent', 'Task Notes']
             task_writer = csv.DictWriter(csvfile, fieldnames=field_names)
 
             if self.check_file_is_empty(self.filename):
@@ -30,6 +32,7 @@ class AddTask(Task):
             task_writer.writerow({
                 'Task Date': task_date,
                 'Task Title': task_title,
+                'Time Spent': task_time_spent,
                 'Task Notes': task_notes
             })
 
@@ -55,11 +58,27 @@ class SearchTask(Task):
             elif search_selection == 4:
                  self.search_by_patter()
 
+    def show_results(self, data_dict):
+        print(data_dict)
+        print(len(data_dict))
+
     def search_by_date(self):
         print("date")
 
     def search_by_time_spent(self):
-        print("spent")
+        data_dict = {}
+        x = 0
+        with open(self.filename, newline='') as csvfile:
+            task_reader = csv.DictReader(csvfile, delimiter=',')
+            for row in task_reader:
+                for key, value in row.items():
+                    if key == " Time Spent":
+                        found = re.search(r'18', row[key])
+                        if found:
+                            data_dict[x] = row
+                            x += 1
+
+        self.show_results(data_dict)
 
     def search_by_exact_match(self):
         print("match")
