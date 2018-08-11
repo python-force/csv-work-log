d3ab41dbb3eb784bbb2cddbcb1a039722eaefc1c
python-force <studio@pythonforce.com>
2018-08-11 13:42:26 -0400

    initial menu loading, functions for validation the selection

---
+++ work-log.py
@@ -0,0 +1,58 @@
+import os
+import time
+
+def clear_screen():
+    """
+    Clear screen
+    :return:
+    """
+    if os.system == "nt":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def job_selected():
+    """
+    Job method selection
+    :return:
+    """
+    selection = input("Hello Commander, what we doing today? "
+                "Please choose a number: ")
+    if check_selection(selection):
+        print("Great choice, " + str(selection) + " it is then.")
+        return int(selection)
+
+def check_selection(selection):
+    """
+    Validation of the user's choice of the job
+    :param enc:
+    :return:
+    """
+    try:
+        int(selection)
+        return True
+    except:
+        pass
+
+# Script doesn't execute when imported
+if __name__ == '__main__':
+    # The script will keep running till the user is satisfied
+    while True:
+        clear_screen()
+
+        # job to do
+        i = job_selected()
+
+        # validation if the input is a number (int)
+        if isinstance(i, int):
+
+            print("Job done.")
+            break
+
+        else:
+            # The selection is not a number, delay for 2 secs
+            # with explanation and ask again
+            print("That choice is not in the binder, please try again. Loading menu...")
+            time.sleep(3)
+
+    print("Done Coding - No more messages to send")
